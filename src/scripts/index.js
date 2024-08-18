f8910c1c41cdfe5041b55fdeed5a248c9ec72742
Thiago Souza <thgdsztls@gmail.com>
2024-08-17 22:36:49 -0300

    scroll

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -22,3 +22,12 @@ function handleOutsideClick(event) {
         hideSidebar();
     }
 }
+
+window.addEventListener("scroll", function() {
+    const nav = document.querySelector("nav");
+    if (window.scrollY > 0) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
