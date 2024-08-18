9cbf7e792f026aedb8c8ac4be3d3393be977717e
Thiago Souza <thgdsztls@gmail.com>
2024-08-17 22:34:27 -0300

    menu correção

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,24 @@
 function showSidebar() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "flex";
+
+    // Adiciona o evento para fechar o sidebar ao clicar fora dele
+    setTimeout(() => {
+        document.addEventListener("click", handleOutsideClick);
+    }, 0);
 }
 
 function hideSidebar() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "none";
+
+    // Remove o evento ao fechar o sidebar
+    document.removeEventListener("click", handleOutsideClick);
+}
+
+function handleOutsideClick(event) {
+    const sidebar = document.querySelector(".sidebar");
+    if (!sidebar.contains(event.target)) {
+        hideSidebar();
+    }
 }
